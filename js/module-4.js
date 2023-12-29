222765f079b9fac3f5deaf5c4cb85dd4496509e3
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2023-12-29 16:28:28 +0200

    mod-4-29/12

--- js/module-4.js
+++ js/module-4.js
@@ -305,3 +305,168 @@ console.log(atTheOldToad.addPotion("Power potion"));
 console.log(atTheOldToad.addPotion("Invisibility"));
 
 // Методи об'єкта
+
+
+// Доступ до властивостей об'єкта. Ключове слово this
+
+const atTheOldToad = {
+  potions: ["Speed potion", "Stone skin"],
+  getPotions() {
+    return this.potions;
+  },
+};
+
+console.log(atTheOldToad.getPotions());
+
+// Доступ до властивостей об'єкта. Ключове слово this
+
+// Зміна за посиланням
+
+const atTheOldToad = {
+  potions: ["Speed potion", "Stone skin"],
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(potionName) {
+   this.potions.push(potionName)},
+};
+
+console.log(atTheOldToad.addPotion("Invisibility"));
+console.log(atTheOldToad.addPotion("Power potion"));
+console.log(atTheOldToad.getPotions());
+
+// Зміна за посиланням
+
+// Масив об’єктів
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Stone skin", price: 520 },
+  ],
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    this.potions.push(newPotion);
+  },
+
+  getTotalPrice() {
+let totalPrice = 0;
+for (const potion of this.potions) {
+totalPrice += potion.price}
+return totalPrice;
+},
+};
+
+console.log(atTheOldToad.addPotion({ name: "Invisibility", price: 620 }));
+console.log(atTheOldToad.getTotalPrice());
+console.log(atTheOldToad.getPotions());
+
+// Масив об’єктів
+
+// Зміна об'єкта в масиві
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Stone skin", price: 520 },
+  ],
+  getPotions() {
+    return this.potions;
+  },
+  updatePotionName(oldName, newName) {
+  for (const potion of this.potions) {
+   if (potion.name === oldName){
+   potion.name = newName}}},
+};
+
+console.log(atTheOldToad.updatePotionName("Speed potion", "Polymorth"));
+console.log(atTheOldToad.updatePotionName("Stone skin", "Invisibility"));
+console.log(atTheOldToad.getPotions());
+
+// Зміна об'єкта в масиві
+
+// Синтаксис spread і rest. Залишкові параметри
+
+function add(...args) {
+let sum = 0;
+    for (const arg of args) {
+    sum += arg;
+    }
+    return sum;
+}
+
+console.log(add(15, 27));
+console.log(add(12, 4, 11, 48));
+console.log(add(32, 6, 13, 19, 8));
+
+// Синтаксис spread і rest. Залишкові параметри
+
+// Збір частини аргументів
+
+function addOverNum(value, ...args) {
+let totalSum = 0;
+for (const arg of args) {
+if (arg > value) {
+totalSum += arg} 
+}
+return totalSum 
+}
+
+console.log(addOverNum(15, 32, 6, 13, 19, 8));
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+
+// Збір частини аргументів
+
+// Входження параметрів
+
+function getExtremeScores(scores) {
+const number = {
+best: Math.max(...scores),
+worst: Math.min(...scores),
+}
+    return number
+}
+
+console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26]));
+console.log(getExtremeScores([19, 7, 4, 17, 81, 24]));
+
+// Входження параметрів
+
+// Створення масиву
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+console.log(allScores);
+console.log(bestScore);
+console.log(worstScore);
+
+// Створення масиву
+
+c
+
+const defaultSettings = {
+  theme: "light",
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+
+const finalSettings = { ...defaultSettings, ...overrideSettings };
+
+console.log(finalSettings);
+
+// Створення об'єкта
